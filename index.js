6db4bebfb49b9c8cfdb9c866daa94f8612fa0428
Martin Muriuki <helheim254@gmail.com>
2023-06-16 11:24:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    const win = record.find(record => record.result === 'W');
+    if (win) {
+        return win.year;
+    }
+    else {
+        return undefined;
+    }
+}
+
